a75c90bf5f7c1d85e8af954baaf259c792f15a58
Shayan Pooya <shayan@chango.com>
2014-01-10 15:42:29 -0500

    First Commit

---
+++ bisect_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+OUT=/tmp/out
+DISCO_PATH=/home/shayan/disco
+
+cd $DISCO_PATH
+COMMIT_SHA=$(git log -1 | head -n 1 | cut -f2 -d' ')
+echo $COMMIT_SHA
+
+sudo make install || exit 1
+while read node; do
+    echo ssh $node 'cd $DISCO_PATH  && git checkout $COMMIT_SHA && sudo make install-node && cd $DISCO_PATH/lib && sudo python setup.py install'
+done < $DISCO_PATH/nodes
+
+disco start || exit 1
+
+for i in 1
+do
+    while read test; do
+      echo "disco test $test"
+      disco test $test 2>$OUT
+      tail -n 1 $OUT | grep OK
+      if [ $? -ne 0 ]
+      then
+        exit 1
+      fi
+    done < $DISCO_PATH/testcases
+done
+
+pgrep beam | xargs kill
