283470b3d3a888a5b4cb998449eb454709cf283a
Shayan Pooya <shayan@chango.com>
2014-02-13 13:23:18 -0500

    Whitespace cleanup

--- disco_tests.sh
+++ disco_tests.sh
@@ -27,10 +27,10 @@ python testcases.py | cut -d'.' -f1 | sort | uniq | while read test; do
   grep -q $test ../../shayan/blacklist
   if [ $? -eq 0 ]
   then
-      echo "Skipping test $test "
+      echo "Skipping test $test"
       continue
   fi
-  echo -n "disco test $test"
+  echo -n "disco test $test "
   disco test $test 2>$OUT
   tail -n 1 $OUT | grep OK
   if [ $? -ne 0 ]
