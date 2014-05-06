2673d19c7f429fed082b040a05876061e9fd271f
Shayan Pooya <shayan@chango.com>
2014-05-06 15:56:38 +0000

    Add a script that sets erlang cookies on all of the nodes in the cluster.

---
+++ cookie.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Usage: ./cookie.sh nodes
+
+# where nodes is a file that includes the name of the slave nodes.
+
+for node in $(cat $1)
+do
+    echo $node
+    ssh $node "yes | rm ~/.erlang.cookie" || exit 1
+    scp ~/.erlang.cookie $node: || exit 1
+done
