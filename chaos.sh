762944719ab73090ccd74b2f17fb071997156622
Shayan Pooya <shayan@chango.com>
2014-06-02 10:30:33 -0500

    add a chaos creator.
    This script can be used to randomly kill a worker on a node.

---
+++ chaos.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+Pids=$(ps -eo pid,fname,cmd | grep worker | grep -v grep | cut -d' ' -f1)
+nPids=$(echo $Pids | wc -w)
+if [ $nPids != 0 ]
+then
+    rand=$(($RANDOM % $nPids + 1))
+    Pid=$(echo $Pids | cut -d' ' -f$rand)
+    echo "Killing pid: " $Pid
+    kill $Pid
+    sleep 1
+else
+    echo "nothing to kill"
+fi
