f12dd2b58db561d42d4ed3b6b6ba77627cf81add
Shayan Pooya <shayan@liveve.org>
2014-06-04 17:17:25 -0400

    chaos: make the regex more specific.

--- chaos.sh
+++ chaos.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-Pids=$(ps aux | grep 'disco.*worker' | grep -v grep | awk '{print $2}')
+Pids=$(ps aux | grep 'disco.*worker.*py' | grep -v grep | awk '{print $2}')
 nPids=$(echo $Pids | wc -w)
 if [ $nPids != 0 ]
 then
